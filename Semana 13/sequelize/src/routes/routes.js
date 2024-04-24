66d4d7d7074e95d0fa363f81b1635fbc71dbec36
diegocmps <diegobarreiros@hotmail.com>
2024-04-24 19:51:17 -0300

    exclusão da rota professor

--- Semana 13/sequelize/src/routes/routes.js
+++ Semana 13/sequelize/src/routes/routes.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const alunoRoutes = require("./alunos.route");
 const cursoRoutes = require("./cursos.route");
-const professoresRoutes = require("./professores.route");
 const loginRoutes = require("./login.route");
 const matriculaRoutes = require("./matricula.route");
 
@@ -10,7 +9,6 @@ const routes = Router()
 routes.use ('/matriculas', matriculaRoutes)
 routes.use('/alunos', alunoRoutes)
 routes.use('/cursos', cursoRoutes)
-routes.use('/professor', professoresRoutes)
 routes.use('/login', loginRoutes)
 
 
