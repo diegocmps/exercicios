9eabc243d30e303050c8fefb48984001d843b15d
diegocmps <diegobarreiros@hotmail.com>
2024-04-24 19:39:18 -0300

    adicionar validacoes de matriculas

--- Semana 13/sequelize/src/controllers/MatriculaController.js
+++ Semana 13/sequelize/src/controllers/MatriculaController.js
@@ -1,5 +1,7 @@
 const Aluno = require("../models/Aluno")
 const Matricula = require("../models/Matricula")
+const Curso = require("../models/Cursos")
+
 
 class MatriculaController {
 
@@ -21,9 +23,25 @@ class MatriculaController {
 
             const aluno = await Aluno.findByPk(aluno_id)
 
-            if(!aluno){
-                return res.status(404).json({message: "Aluno não encontrado."})
+            if (!aluno) {
+                return res.status(404).json({ message: "Aluno não encontrado." })
             }
+
+            const curso = await Curso.findByPk(curso_id)
+            if (!curso) return res.status(404).json({ message: "O curso não existe." })
+
+            //* Verificar se já existe aluno cadastrado no curso *//
+            const matriculaExistente = await Matricula.findOne({
+                where: {
+                    curso_id: curso_id,
+                    aluno_id: aluno_id
+                }
+            })
+
+            if (matriculaExistente) {
+                return res.status(409).json({ message: "Aluno já cadastrado para este curso." })
+            }
+            //*----------------*//
 
             const matricula = await Matricula.create({
                 curso_id: curso_id,
@@ -31,7 +49,6 @@ class MatriculaController {
             })
 
             res.status(201).json(matricula)
-
 
         } catch (error) {
             res.status(500).json({ message: "Não foi possível realizar o cadastro." })
